1fb1fc95c595d8c70ae5ec36f40b06a88129f31a
tyomhak <aghakobyan@gmail.com>
2021-01-28 09:49:00 +0400

    Added solutions for Section 1.2

---
+++ Chapter-1/ex1_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	int v1 = 0, v2 = 0;
+	std::cout << "Please enter two numbers: ";
+	std::cin >> v1 >> v2;
+	std::cout << "The difference between ";
+	std::cout << v1;
+	std::cout << " and ";
+	std::cout << v2;
+	std::cout << " is equal to ";
+	std::cout << v1 - v2;
+	std::cout << std::endl;
+	return 0; 
+
+}
