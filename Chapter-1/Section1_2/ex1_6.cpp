655fd40daa8c6aad7a4965e43af203978f61af78
tyomhak <aghakobyan@gmail.com>
2021-02-09 17:30:12 +0400

    moved solution src files to section folders

---
+++ Chapter-1/Section1_2/ex1_6.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "The sum of " << v1; 	// need to remove `;' character
+		<< " and " << v2;		// need to remove `:' character
+		<< " is " << v1 + v2 << std::endl;
+}
