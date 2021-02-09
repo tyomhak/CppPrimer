ac92d46b3e9724579bc9cc2fd0e83579a194e7f4
tyomhak <aghakobyan@gmail.com>
2021-02-09 18:06:11 +0400

    delete extra src copied

--- Chapter-1/ex1_6.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "The sum of " << v1; 	// need to remove `;' character
-		<< " and " << v2;		// need to remove `:' character
-		<< " is " << v1 + v2 << std::endl;
-}
